mtisza/drbd

commit b29dba31e92973fe4442b2c08f4d151730f9de89

drbd: flush_send_buffer() result needs to be checked
alloc_send_buffer() may implicitly flush accumulated data
if there is not enough room to accomodate new data.

It must not ignore potential network failures.

Now alloc_send_buffer() may also return an ERR_PTR.
Callers need to check with IS_ERR and handle errors as appropriate.

drbd/drbd_main.c
@@ -910,14 +910,17 @@ have_page:
 	sbuf->pos = page_address(sbuf->page);
 }
 
-static char *alloc_send_buffer(struct drbd_connection *connection, int size,
+static char * __must_check alloc_send_buffer(struct drbd_connection *connection, int size,
 			       enum drbd_stream drbd_stream)
 {
 	struct drbd_send_buffer *sbuf = &connection->send_buffer[drbd_stream];
 	char *page_start = page_address(sbuf->page);
+	int err;
 
 	if (sbuf->pos - page_start + size > PAGE_SIZE) {
-		flush_send_buffer(connection, drbd_stream);
+		err = flush_send_buffer(connection, drbd_stream);
+		if (err)
+			return ERR_PTR(err);
 		new_or_recycle_send_buffer_page(sbuf);
 	}
 
@@ -957,6 +960,7 @@ void *__conn_prepare_command(struct drbd_connection *connection, int size,
 {
 	struct drbd_transport *transport = &connection->transport;
 	int header_size;
+	void *p;
 
 	if (connection->cstate[NOW] < C_CONNECTING)
 		return NULL;
@@ -965,7 +969,10 @@ void *__conn_prepare_command(struct drbd_connection *connection, int size,
 		return NULL;
 
 	header_size = drbd_header_size(connection);
-	return alloc_send_buffer(connection, header_size + size, drbd_stream) + header_size;
+	p = alloc_send_buffer(connection, header_size + size, drbd_stream) + header_size;
+	if (IS_ERR(p))
+		return NULL;
+	return p;
 }
 
 /**
@@ -1118,19 +1125,22 @@ void drbd_cork(struct drbd_connection *connection, enum drbd_stream stream)
 	mutex_unlock(&connection->mutex[stream]);
 }
 
-void drbd_uncork(struct drbd_connection *connection, enum drbd_stream stream)
+int drbd_uncork(struct drbd_connection *connection, enum drbd_stream stream)
 {
 	struct drbd_transport *transport = &connection->transport;
 	struct drbd_transport_ops *tr_ops = &transport->class->ops;
+	int err;
 
 	mutex_lock(&connection->mutex[stream]);
-	flush_send_buffer(connection, stream);
-
-	clear_bit(CORKED + stream, &connection->flags);
-	/* only call into transport, if we expect it to work */
-	if (connection->cstate[NOW] >= C_CONNECTING)
-		tr_ops->hint(transport, stream, UNCORK);
+	err = flush_send_buffer(connection, stream);
+	if (!err) {
+		clear_bit(CORKED + stream, &connection->flags);
+		/* only call into transport, if we expect it to work */
+		if (connection->cstate[NOW] >= C_CONNECTING)
+			tr_ops->hint(transport, stream, UNCORK);
+	}
 	mutex_unlock(&connection->mutex[stream]);
+	return err;
 }
 
 int send_command(struct drbd_connection *connection, int vnr,
@@ -1993,10 +2003,14 @@ send_bitmap_rle_or_plain(struct drbd_peer_device *peer_device, struct bm_xfer_ct
 	struct drbd_device *device = peer_device->device;
 	unsigned int header_size = drbd_header_size(peer_device->connection);
 	struct p_compressed_bm *pc;
+	char *p;
 	int len, err;
 
-	pc = (struct p_compressed_bm *)
-		(alloc_send_buffer(peer_device->connection, DRBD_SOCKET_BUFFER_SIZE, DATA_STREAM) + header_size);
+	p = alloc_send_buffer(peer_device->connection, DRBD_SOCKET_BUFFER_SIZE, DATA_STREAM);
+	if (IS_ERR(p))
+		return -EIO;
+
+	pc = (struct p_compressed_bm *)(p + header_size);
 
 	len = fill_bitmap_rle_bits(peer_device, pc,
 			DRBD_SOCKET_BUFFER_SIZE - header_size - sizeof(*pc), c);
@@ -2254,6 +2268,8 @@ static int _drbd_no_send_page(struct drbd_peer_device *peer_device, struct page 
 	int err;
 
 	buffer2 = alloc_send_buffer(connection, size, DATA_STREAM);
+	if (IS_ERR(buffer2))
+		return PTR_ERR(buffer2);
 	from_base = kmap_atomic(page);
 	memcpy(buffer2, from_base + offset, size);
 	kunmap_atomic(from_base);
@@ -2274,9 +2290,12 @@ static int _drbd_send_bio(struct drbd_peer_device *peer_device, struct bio *bio)
 	struct drbd_connection *connection = peer_device->connection;
 	struct bio_vec bvec;
 	struct bvec_iter iter;
+	char *p;
 
 	/* Flush send buffer and make sure PAGE_SIZE is available... */
-	alloc_send_buffer(connection, PAGE_SIZE, DATA_STREAM);
+	p = alloc_send_buffer(connection, PAGE_SIZE, DATA_STREAM);
+	if (IS_ERR(p))
+		return PTR_ERR(p);
 	connection->send_buffer[DATA_STREAM].allocated_size = 0;
 
 	/* hint all but last page with MSG_MORE */
@@ -2324,11 +2343,13 @@ static int _drbd_send_zc_bio(struct drbd_peer_device *peer_device, struct bio *b
 		struct drbd_transport_ops *tr_ops = &transport->class->ops;
 		int err;
 
-		flush_send_buffer(connection, DATA_STREAM);
+		err = flush_send_buffer(connection, DATA_STREAM);
 
-		err = tr_ops->send_zc_bio(transport, bio);
-		if (!err)
-			peer_device->send_cnt += bio->bi_iter.bi_size >> 9;
+		if (!err) {
+			err = tr_ops->send_zc_bio(transport, bio);
+			if (!err)
+				peer_device->send_cnt += bio->bi_iter.bi_size >> 9;
+		}
 
 		return err;
 	}
@@ -2342,7 +2363,10 @@ static int _drbd_send_zc_ee(struct drbd_peer_device *peer_device,
 	unsigned len = peer_req->i.size;
 	int err;
 
-	flush_send_buffer(peer_device->connection, DATA_STREAM);
+	err = flush_send_buffer(peer_device->connection, DATA_STREAM);
+	if (err)
+		return err;
+
 	/* hint all but last page with MSG_MORE */
 	page_chain_for_each(page) {
 		unsigned l = min_t(unsigned, len, PAGE_SIZE);
@@ -3932,6 +3956,7 @@ void drbd_transport_shutdown(struct drbd_connection *connection, enum drbd_tr_fr
 	mutex_lock(&connection->mutex[DATA_STREAM]);
 	mutex_lock(&connection->mutex[CONTROL_STREAM]);
 
+	/* Ignore send errors, if any: we are shutting down. */
 	flush_send_buffer(connection, DATA_STREAM);
 	flush_send_buffer(connection, CONTROL_STREAM);
 

drbd/drbd_int.h
@@ -1870,7 +1870,7 @@ int drbd_bmio_clear_one_peer(struct drbd_device *device,
 bool drbd_device_stable(struct drbd_device *device, u64 *authoritative_ptr);
 void drbd_flush_peer_acks(struct drbd_resource *resource);
 void drbd_cork(struct drbd_connection *connection, enum drbd_stream stream);
-void drbd_uncork(struct drbd_connection *connection, enum drbd_stream stream);
+int drbd_uncork(struct drbd_connection *connection, enum drbd_stream stream);
 void drbd_open_counts(struct drbd_resource *resource, int *rw_count_ptr,
 		      int *ro_count_ptr);
 

drbd/drbd_receiver.c
@@ -11339,11 +11339,11 @@ void drbd_send_acks_wf(struct work_struct *ws)
 	err = drbd_finish_peer_reqs(connection);
 
 	/* but unconditionally uncork unless disabled */
-	if (tcp_cork)
-		drbd_uncork(connection, CONTROL_STREAM);
-
 	if (err)
 		change_cstate(connection, C_NETWORK_FAILURE, CS_HARD);
+	else if (tcp_cork)
+		drbd_uncork(connection, CONTROL_STREAM);
+
 }
 
 void drbd_send_peer_ack_wf(struct work_struct *ws)

drbd/drbd_sender.c
@@ -3358,8 +3358,10 @@ static void wait_for_sender_todo(struct drbd_connection *connection)
 	nc = rcu_dereference(connection->transport.net_conf);
 	uncork = nc ? nc->tcp_cork : 0;
 	rcu_read_unlock();
-	if (uncork)
-		drbd_uncork(connection, DATA_STREAM);
+	if (uncork) {
+		if (drbd_uncork(connection, DATA_STREAM))
+			return;
+	}
 
 	for (;;) {
 		int send_barrier;

commit 4d1bf808a8b2c2307cacb39daa1e4e5bcaf004b3

drbd: __conn_prepare_command(): check ERR_PTR before adding header size
alloc_send_buffer() may return ERR_PTR(err) when its implicit
flush_send_buffer() fails.

__conn_prepare_command() currently adds header_size before checking
IS_ERR(). That corrupts the error pointer.

  PingAck did not arrive in time.
  conn( Connected -> NetworkFailure ) peer( Secondary -> Unknown )
  sock was shut down by peer
  BUG: kernel NULL pointer dereference, address: 000000000000000b
  Workqueue: drbd_as_pvc-5532afe7-9a drbd_send_acks_wf [drbd]
  RIP: 0010:_drbd_send_ack+0x3f/0xd0 [drbd]
  Call Trace:
   e_end_resync_block+0x67/0x160 [drbd]
   drbd_finish_peer_reqs+0xb3/0x110 [drbd]
   drbd_send_acks_wf+0x6e/0x90 [drbd]

Fix this by checking IS_ERR() on the raw alloc_send_buffer() result
first, and only then advancing past the packet header.

Fixes: b29dba31e929 ("drbd: flush_send_buffer() result needs to be checked")

drbd/drbd_main.c
@@ -939,8 +939,8 @@ void *__conn_prepare_command(struct drbd_connection *connection, int size,
 				    enum drbd_stream drbd_stream)
 {
 	struct drbd_transport *transport = &connection->transport;
+	char *p;
 	int header_size;
-	void *p;
 
 	if (connection->cstate[NOW] < C_CONNECTING)
 		return NULL;
@@ -949,10 +949,10 @@ void *__conn_prepare_command(struct drbd_connection *connection, int size,
 		return NULL;
 
 	header_size = drbd_header_size(connection);
-	p = alloc_send_buffer(connection, header_size + size, drbd_stream) + header_size;
+	p = alloc_send_buffer(connection, header_size + size, drbd_stream);
 	if (IS_ERR(p))
 		return NULL;
-	return p;
+	return p + header_size;
 }
 
 /**